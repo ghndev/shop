234d9729b078c3bffdb8fe16ed3e5ea1c9b55473
Kwak Hoon <77324409+hooncdn@users.noreply.github.com>
2024-06-26 15:22:40 +0900

    Add uploadthing

--- app/configure/layout.tsx
+++ app/configure/layout.tsx
@@ -1,9 +1,11 @@
 import MaxWidthWrapper from '@/components/max-width-wrapper'
+import Steps from '@/components/steps'
 import { ReactNode } from 'react'
 
 export default function ConfigureLayout({ children }: { children: ReactNode }) {
   return (
     <MaxWidthWrapper className="flex flex-1 flex-col">
+      <Steps />
       {children}
     </MaxWidthWrapper>
   )
---
+++ components/steps.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+
+export default function Steps() {
+  const pathname = usePathname()
+  return <div></div>
+}
